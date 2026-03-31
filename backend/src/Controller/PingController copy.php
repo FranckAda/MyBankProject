4181a5d7b3a2a93feb9a4bb710c79d5ddc3c0684
Franck <162502379+Franckad1@users.noreply.github.com>
2026-03-31 08:34:31 +0200

    Replace the files name

---
+++ backend/src/Controller/PingController copy.php
@@ -0,0 +1,16 @@
+<?php
+
+// namespace App\Controller;
+
+// use Symfony\Bundle\FrameworkBundle\Controller\AbstractController;
+// use Symfony\Component\HttpFoundation\JsonResponse;
+// use Symfony\Component\Routing\Attribute\Route;
+
+// final class PingControl extends AbstractController
+// {
+//   #[Route('/api/healthy', name: 'api_healthy', methods: ['GET'])]
+//   public function __invoke(): JsonResponse
+//   {
+//     return $this->json(['Symfoni API status' => "up"]);
+//   }
+// }
