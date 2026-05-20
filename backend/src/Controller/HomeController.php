2a6dc1eee5d8fb482ba25f2d98b8cf86703bbb24
Franck <162502379+Franckad1@users.noreply.github.com>
2026-05-20 15:18:52 +0200

    fix(api):Changer le nom d'un chemin

--- backend/src/Controller/HomeController.php
+++ backend/src/Controller/HomeController.php
@@ -8,7 +8,7 @@ use Symfony\Component\Routing\Attribute\Route;
 
 class HomeController extends AbstractController
 {
-  #[Route('/home', name: 'home', methods: ['GET'])]
+  #[Route('/api/home', name: 'home', methods: ['GET'])]
   public function index(): JsonResponse
   {
     return new JsonResponse(['status' => 'MyBank API is running']);
